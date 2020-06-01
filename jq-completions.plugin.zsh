7fcc3e858f9e4e7edefc606d2c9d608ad445a3e5
Arnaud Lauret <arno.di.loreto@gmail.com>
2020-06-01 14:45:19 +0200

    Capitalized descriptions. Multiple --arg and --argjson name value handled

--- jq-completions.plugin.zsh
+++ jq-completions.plugin.zsh
@@ -2,13 +2,13 @@ function _jq {
     local line
 
     _arguments -C \
-        {-f,--from-file}"[read filter from the a rather than from a command line]:file:_files" \
-        {-Ldirectory,-L}"[Prepend directory to the search list for modules]":dir:_dirs \
-        "--arg[set variable \$a to value <v>]": \
-        "--argjson[set variable \$a to JSON value <v>]" \
-        "--slurpfile[set variable \$a to an array of JSON texts read from <f>]" \
-        "--args[Remaining arguments are positional string arguments.]" \
-        "--jsonargs[Remaining arguments are positional JSON text arguments]" \
+        {-f,--from-file}"[Read filter from a file rather than from a command line.]:file:_files" \
+        {-Ldirectory,-L}"[Prepend directory to the search list for modules]:directory:_directories" \
+        "*--arg[Usage --arg a v. Set variable \$a to value <v>. Also available as \$ARGS.named\[a\].]:name::value:" \
+        "*--argjson[Usage --argjson a v. Set variable \$a to JSON value <v>. Also available as \$ARGS.named\[a\].]:name::value:" \
+        "--slurpfile[Usage --slurpfile a f. Set variable \$a to an array of JSON texts read from file <f>.]:file:_files" \
+        "--args[Usage --args v1 v2. Positional string arguments available as \$ARGS.positional\[\].]" \
+        "--jsonargs[Positional JSON arguments available as \$ARGS.positional\[\].]" \
         "--version[Output the jq version and exit with zero.]" \
         "--seq[Use the application/json-seq MIME type scheme for separating JSON texts in jq's input and output.]" \
         "--stream[Parse  the  input  in  streaming fashion, outputing arrays of path and leaf values (scalars and empty arrays or empty objects).]" \
@@ -19,13 +19,13 @@ function _jq {
         "--tab[Use a tab for each indentation level instead of two spaces.]" \
         "--indent[Use the given number of spaces (no more than 8) for indentation.]:int:" \
         {--color-output,-C}"[colorize JSON.]" \
-        {--monochrome-output,-M}"[don't colorize JSON.]" \
-        {--ascii-output,-a}"[force pure ASCII output.]" \
+        {--monochrome-output,-M}"[Don't colorize JSON.]" \
+        {--ascii-output,-a}"[Force pure ASCII output.]" \
         "--unbuffered[Flush the output after each JSON object is printed.]" \
         {--sort-keys,-S}"[Output the fields of each object with the keys in sorted order.]" \
-        {--raw-output,-r}"[output raw strings, not JSON texts.]" \
+        {--raw-output,-r}"[Output raw strings, not JSON texts.]" \
         {--join-output,-j}"[Like -r but jq won't print a newline after each output.]" \
-        {-e,--exit-status}"[Set exit status based on output value.]" \
+        {-e,--exit-status}"[Set exit status based on output valueqq.]" \
         "*:file:_files"
 }
 
